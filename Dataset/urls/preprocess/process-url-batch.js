ec8e866337b49cd997f976688fbdbede6a9136a5
LokeshPatil-loki <lokesh1404@outlook.com>
2023-02-19 23:29:07 +0530

    Added batch 4 of 400 urls

--- Dataset/urls/preprocess/process-url-batch.js
+++ Dataset/urls/preprocess/process-url-batch.js
@@ -1,15 +1,15 @@
-import urlBatch from "./url-batch-400-600.json" assert { type: "json" };
+import urlBatch from "./url-batch-600-1000.json" assert { type: "json" };
 import fs from "fs";
 
 const urlArray = urlBatch.map(urlItem => urlItem.url);
-const urlGroup1 = urlArray.splice(0,50);
-const urlGroup2 = urlArray.splice(0,50);
+const urlGroup1 = urlArray.splice(0,200);
+const urlGroup2 = urlArray.splice(0,100);
 const urlGroup3 = urlArray.splice(0,50);
 const urlGroup4 = urlArray.splice(0,50);
 
-fs.writeFileSync("./batch-4[200]/url-group-1.json", JSON.stringify(urlGroup1));
-fs.writeFileSync("./batch-4[200]/url-group-2.json", JSON.stringify(urlGroup2));
-fs.writeFileSync("./batch-4[200]/url-group-3.json", JSON.stringify(urlGroup3));
-fs.writeFileSync("./batch-4[200]/url-group-4.json", JSON.stringify(urlGroup4));
+fs.writeFileSync("./batch-4[400]/url-group-1.json", JSON.stringify(urlGroup1));
+fs.writeFileSync("./batch-4[400]/url-group-2.json", JSON.stringify(urlGroup2));
+fs.writeFileSync("./batch-4[400]/url-group-3.json", JSON.stringify(urlGroup3));
+fs.writeFileSync("./batch-4[400]/url-group-4.json", JSON.stringify(urlGroup4));
 
 
